cceec7653b3e49d44651f55cc9909cfbb753be96
Altagracia Cierra <alcierra@student.21-school.ru>
2022-11-15 21:54:13 +0300

    ex02 main

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
+#include "Fixed.hpp"
+
+int main( void )
+{
+	Fixed		a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << std::endl << "=== ++ test ===" << std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << std::endl << "=== min/max ===" << std::endl;
+	std::cout << "a:\t" << a << std::endl;
+	std::cout << "b:\t" << b << std::endl;
+	std::cout << "max:\t" << Fixed::max( a, b ) << std::endl;
+	std::cout << "min:\t" << Fixed::min( a, b ) << std::endl;
+
+	std::cout << std::endl << "=== -- test ===" << std::endl;
+	std::cout << a << std::endl;
+	std::cout << --a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << std::endl << "=== c/d test ===" << std::endl;
+	Fixed		c( 100 );
+	Fixed		d( 42.42f );
+	std::cout << "c:\t" << c << std::endl;
+	std::cout << "d:\t" << d << std::endl;
+	std::cout << c << " < " << d << ":\t " << (c < d) << std::endl;
+	std::cout << c << " > " << d << ":\t " << (c > d) << std::endl;
+	std::cout << c << " <= " << d << ":\t " << (c <= d) << std::endl;	std::cout << "a:\t" << a << std::endl;
+	std::cout << "b:\t" << b << std::endl;
+	std::cout << c << " >= " << d << ":\t " << (c >= d) << std::endl;
+	std::cout << c << " == " << d << ":\t " << (c == d) << std::endl;
+	std::cout << c << " != " << d << ":\t " << (c != d) << std::endl;
+	std::cout << c << " + " << d << ":\t " << (c + d) << std::endl;
+	std::cout << c << " - " << d << ":\t " << (c - d) << std::endl;
+	std::cout << c << " * " << d << ":\t " << (c * d) << std::endl;
+	std::cout << c << " / " << d << ":\t " << (c / d) << std::endl;
+
 	return 0;
 }
